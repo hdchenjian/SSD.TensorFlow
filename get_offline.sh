a75403feccdfc022232f4a5141b2a690cb78783b
chen jian <jianchen@iim.ltd>
2020-03-17 19:09:46 +0800

    fix typo

--- get_offline.sh
+++ get_offline.sh
@@ -1,4 +1,4 @@
-./pb_to_cambricon --graph=ssd300_vgg16_mlu.pb \
+../tools/pb_to_cambricon/pb_to_cambricon.host --graph=model/ssd300_vgg16_mlu.pb \
                   --param_file=ssd_model_offline_param_file.txt \
                   --core_version="MLU270" \
                   --core_num=1 \
